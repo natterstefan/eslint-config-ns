aaee85e50d3b05b2fc799fe65bf4b6731c5acbc8
Stefan Natter <stefan@natter.at>
2022-02-26 18:23:53 +0100

    feat: optimised prepublish script

---
+++ scripts/prepublish.js
@@ -0,0 +1,14 @@
+var copyfiles = require('copyfiles')
+
+const filesToCopy = ['LICENSE']
+
+const targetPackages = [
+  'packages/eslint-config-ns/',
+  'packages/eslint-config-ns-base/',
+  'packages/eslint-config-ns-ts/',
+  'packages/eslint-config-ns-ts-base/',
+]
+
+targetPackages.forEach(targetPath => {
+  copyfiles([...filesToCopy, targetPath], () => null)
+})
