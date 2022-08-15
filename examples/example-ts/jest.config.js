d756a7686c052c863c805a692b861006b2f0b73f
Stefan Natter <stefan@natter.at>
2022-08-15 08:42:23 +0200

    test: added config tests

--- examples/example-ts/jest.config.js
+++ examples/example-ts/jest.config.js
@@ -1,7 +1,13 @@
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
 module.exports = {
   preset: 'jest-preset-ns/presets/default',
   globals: {
-    // @see https://github.com/natterstefan/jest-preset-ns/issues/4#issuecomment-765416205
+    /**
+     * @see https://github.com/natterstefan/jest-preset-ns/issues/4#issuecomment-765416205
+     * @type {import('ts-jest').GlobalConfigTsJest["ts-jest"]}
+     */
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
     },
