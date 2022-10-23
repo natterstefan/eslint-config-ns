f9772607bf8252d7c9d6338758841721b3c322bb
Stefan Natter <stefan@natter.at>
2022-10-23 11:31:30 +0200

    chore: updated jest and its dependencies

--- examples/example-ts/jest.config.js
+++ examples/example-ts/jest.config.js
@@ -3,13 +3,16 @@
  */
 module.exports = {
   preset: 'jest-preset-ns/presets/default',
-  globals: {
-    /**
-     * @see https://github.com/natterstefan/jest-preset-ns/issues/4#issuecomment-765416205
-     * @type {import('ts-jest').GlobalConfigTsJest["ts-jest"]}
-     */
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      /**
+       * @see https://github.com/natterstefan/jest-preset-ns/issues/4#issuecomment-765416205
+       * @type {import('ts-jest').GlobalConfigTsJest["ts-jest"]}
+       */
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
   },
 }
