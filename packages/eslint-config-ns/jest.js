12c6fb542c7903b6001ff3a32c6d1cb70908d7b9
Stefan Natter <stefan@natter.at>
2025-03-08 20:21:54 +0100

    feat: moved files into eslint-config-ns

--- packages/eslint-config-ns/jest.js
+++ packages/eslint-config-ns/jest.js
@@ -2,7 +2,7 @@
  * @type {import('eslint').ESLint.Options}
  */
 module.exports = {
-  extends: ['eslint-config-ns-base/jest'],
+  extends: ['./rules/jest'],
   parser: '@babel/eslint-parser',
   parserOptions: {
     requireConfigFile: false,
