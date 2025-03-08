e73409f785d7f51f9ed5b60b6584aeda41c6c129
Stefan Natter <stefan@natter.at>
2025-03-08 20:19:37 +0100

    feat: one more step towards one unified package

--- packages/eslint-config-ns/react.js
+++ packages/eslint-config-ns/react.js
@@ -1,22 +1,8 @@
 /**
- * Enables ESLint to use dependencies of the shared config
- * @see https://github.com/eslint/eslint/issues/3458
- */
-require('./patch/modern-module-resolution')
-
-/**
  * @type {import('eslint').ESLint.Options}
  */
 module.exports = {
-  extends: ['./rules/react', './rules/react-a11y', './rules/react-hooks'].map(
-    require.resolve,
-  ),
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    babelOptions: {
-      presets: ['@babel/preset-react'],
-    },
-    requireConfigFile: false,
-  },
+  extends: ['./rules/react', './rules/react-a11y', './rules/react-hooks'],
+  parser: '@typescript-eslint/parser',
   rules: {},
 }
--- packages/eslint-config-ns/storybook.js
+++ packages/eslint-config-ns/storybook.js
@@ -1,15 +1,9 @@
 /**
- * Enables ESLint to use dependencies of the shared config
- * @see https://github.com/eslint/eslint/issues/3458
- */
-require('./patch/modern-module-resolution')
-
-/**
  * @see https://github.com/storybookjs/eslint-plugin-storybook
  * @type {import('eslint').ESLint.Options}
  */
 module.exports = {
-  extends: ['./rules/storybook'].map(require.resolve),
+  extends: ['./rules/storybook'],
   parser: '@babel/eslint-parser',
   parserOptions: {
     requireConfigFile: false,
